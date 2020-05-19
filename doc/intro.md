c6d563e7c782c406dd7ca80f05b510853acc2c63
Bob Poekert <bob@poekert.com>
2020-05-19 14:08:10 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fannypack
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fannypack/core_test.clj
@@ -0,0 +1,7 @@
+(ns fannypack.core-test
+  (:require [clojure.test :refer :all]
+            [fannypack.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
